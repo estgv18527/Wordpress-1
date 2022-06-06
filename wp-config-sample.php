0bd1a8ace7f738f0c31cba17cd0f4dc39fe0a1cc
estgv18527 <106957718+estgv18527@users.noreply.github.com>
2022-06-06 13:02:21 +0100

    Update wp-config-sample.php

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -20,16 +20,16 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'database_name_here' );
+define( 'DB_NAME', 'transgest_Teste' );
 
 /** Database username */
-define( 'DB_USER', 'username_here' );
+define( 'DB_USER', 'transgest_teste' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'password_here' );
+define( 'DB_PASSWORD', 'DiogoTeste123' );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'transgest.pt' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
